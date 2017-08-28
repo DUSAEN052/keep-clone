9368bb251ff8c183cb4fb89c501ce77a4ac43760
11111 <norb662@gmail.com>
2017-08-28 17:38:17 -0400

    Added GET, POST, DELETE routes.

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Note = require('./models/note.js');
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+const notes = new Map();
+let counter = 0;
+
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+  method: 'GET',
+  path: '/notes',
+  handler: function (request, reply) {
+    reply(Array.from(notes));
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/note',
+  handler: function (request, reply) {
+    let note = new Note(request.payload.title, request.payload.body, counter);
+    notes.set(counter, note);
+    counter++;
+    reply(note);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/note/{id}',
+  handler: function (request, reply) {
+    reply(notes.get(parseInt(request.params.id)));
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/note/{id}',
+  handler: function(request, reply) {
+    notes.delete(parseInt(request.params.id));
+    reply().code(204);
+  }
+})
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
